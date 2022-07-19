f8c92af21f9b1c920624e1c71233633e4bcbd335
Joel Martinez <yoyomartinez314@gmail.com>
2022-07-19 14:02:01 +0200

    what ever

--- main.c
+++ main.c
@@ -3,11 +3,49 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+const int n_symbols = 3;
+const int max_freq = 16383; // 2*14 - 1
 static int buffer;
 static int free_bits;
 static int eof;
 static FILE *file;
 
+static int cum_freq[n_symbols]; // length to 64 first bits of file // first lets do symbols 'a', 'b', 'c'
+static int freq[n_symbols]; 
+
+//static int symbol_to_idx[n_symbols]; // symbol to index in freq array
+//static int idx_to_symbol[n_symbols]; // index to symbol in freq array
+
+// utf-8 == index in freq array for now
+// ahora se llama metodo de bisseccion, encodear ahsta que intervalo este enteramente entre los cum_freq
+// de un simbolo
+// quizas añadir cum_frq[n_symbols] = 0
+
+void encode_symbol(int symbol) {
+    
+}
+void start_model() {
+    cum_freq[n_symbols - 1] = 1;
+    freq[n_symbols - 1] = 1;
+    for (int i = n_symbols - 2; i >= 0; i--) {
+        freq[i] = 1;
+        cum_freq[i] = 1 + cum_freq[i + 1]; // first value is the biggest  
+    }
+}
+void update_model(int symbol) {
+    if (cum_freq[0] == max_freq) {
+        freq[n_symbols-1] = (freq[n_symbols-1] + 1) / 2;
+        cum_freq[n_symbols-1] = freq[n_symbols-1] + 0;
+        for (int i = n_symbols-2; i >= 0; i--) {
+            freq[i] = (freq[i] + 1) / 2;
+            cum_freq[i] = freq[i] + cum_freq[i+1];
+        }
+    }
+    freq[symbol]++;
+    for (int i = symbol; i >= 0; i--) {
+        cum_freq[i] += 1;
+    }
+}
 void start_read_bit(const char *filename) {
     file = fopen(filename, "rb");
     free_bits = 8;
@@ -39,6 +77,7 @@ int read_bit() {
         }
     }
     bit = (buffer & 0b10000000) >> 7; // slower but we can now write utf-8 and read it from .txt
+    // for actual implementation, follow acm87 scheme
     buffer <<= 1;
     free_bits++;
     return bit;
